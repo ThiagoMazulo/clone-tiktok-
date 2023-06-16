b5ec38c9c05cb961208d1bb7d902427a4b8f7daa
Thiago Mazulo <thiagomazulo@gmail.com>
2023-06-16 16:57:25 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+
+  return (
+    <>
+    <div className='app'>
+      <h1>Olá mundo!</h1>
+      <div className='app_videos'>
+
+      </div>
+
+    </div>
+      
+    </>
+  )
+}
+
+export default App
